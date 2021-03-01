7a037b8a3a2b8fe95898b3742845fa55baf529c6
Gabriel <rexesgames18@gmail.com>
2021-03-01 20:14:11 +0000

    Zombie Behaviour

---
+++ VR-Zombie-Game/Assets/Scripts/AI/BT/Nodes/Selector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Behaviour Tree Selector Node
+/// </summary>
+public class Selector : Node
+{
+    protected List<Node> nodes = new List<Node>();
+
+    public Selector(List<Node> nodes)
+    {
+        this.nodes = nodes;
+    }
+
+    public override NodeState Evaluate()
+    {
+        foreach (var node in nodes)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.FAILURE:
+                    break;
+                case NodeState.SUCCESS:
+                    state = NodeState.SUCCESS;
+                    return state;
+                case NodeState.RUNNING:
+                    state = NodeState.RUNNING;
+                    return state;
+                default:
+                    break;
+            }
+        };
+
+        state = NodeState.FAILURE;
+        return state;
+    }
+}
---
+++ VR-Zombie-Game/Assets/Scripts/AI/Zombie/Nodes/Idle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : Node
+{
+    private ZombieAI zombie;
+    private Animator anim;
+    private float health;
+    private float deathThreshold = 0;
+
+    public Idle(ZombieAI zombie)
+    {
+        this.zombie = zombie;
+        anim = zombie.Anim;
+    }
+
+    public override NodeState Evaluate()
+    {
+        health = zombie.Health;
+
+        if (zombie.IsPlayerFound == true || health <= deathThreshold)
+        {
+            zombie.IsIdle = false;
+            zombie.IsPatrolling = false;
+            anim.SetBool("isIdle", false);
+            anim.SetBool("IsPatrolling", false);
+            state = NodeState.FAILURE;
+            return state;
+        }
+
+        zombie.IsIdle = true;
+        zombie.IsPatrolling = false;
+        anim.SetBool("isIdle", true);
+        anim.SetBool("IsPatrolling", false);
+        state = NodeState.SUCCESS;
+        return state;
+    }
+}
